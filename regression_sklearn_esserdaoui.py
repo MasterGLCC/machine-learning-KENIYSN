9cd0f470aa1cddefce8fdd95a03b1e4aae7464a8
KeniYsn <assardaouiyassine68@gmail.com>
2026-04-06 22:43:23 +0000

    Regression lineaire (Sklearn)

--- regression_sklearn_esserdaoui.py
+++ regression_sklearn_esserdaoui.py
@@ -4,14 +4,15 @@ from sklearn.linear_model import LinearRegression
 
 dataset = np.genfromtxt('train.csv',delimiter=',', skip_header=1, usecols=(46, 80))
 X=dataset[:,0].reshape(-1,1)
-Y=X=dataset[:,1]
-
+Y=dataset[:,1]
+  #Creation d une instance de l objet LinearRegression
 model = LinearRegression()
+  #on utilisant l'algorithme des Moindres Carrés Ordinaires la méthode fit prend les donner d entrer et les resultat attendus pour apprendre la relation mathématique qui les lie.
 model.fit(X, Y)
 
 y_pred = model.predict(X)
 
-# 4. Affichage des résultats
+  #Affichage des resultats
 plt.scatter(X, Y, color='blue', alpha=0.3, label='Données réelles')
 plt.plot(X, y_pred, color='green', linewidth=3, label='Modèle Sklearn')
 plt.title("Régression Linéaire avec Scikit-Learn")
